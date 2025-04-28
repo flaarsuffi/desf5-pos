1911f4f0f1658ab80bb1c63aace3f3be33bc19f0
Flavia Arsuffi <55116057+flaarsuffi@users.noreply.github.com>
2025-04-28 18:34:23 -0300

    Create ProductsSpringApplication.java

---
+++ src/main/java/com/flaarsuffi/productsapi/ProductsSpringApplication.java
@@ -0,0 +1,13 @@
+// ProductsSpringApplication.java
+
+package com.flaarsuffi.productsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductsSpringApplication.class, args);
+    }
+}
